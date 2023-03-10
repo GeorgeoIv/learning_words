08cb3105c6c395495f7054a1384351f52c1d0633
Geo <geo@Geos-MacBook-Pro.local>
2023-03-10 16:35:08 +0300

    css2.0

--- src/components/Auth/SignInForm.jsx
+++ src/components/Auth/SignInForm.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import SignUpForm from './SignUpForm';
+import React from "react";
+import SignUpForm from "./SignUpForm";
 
 export default function SignInForm() {
   const submitHandler = (e) => {
@@ -7,18 +7,17 @@ export default function SignInForm() {
 
     const data = Object.fromEntries(new FormData(e.target));
 
-    fetch('/api/auth/signin', {
-      method: 'POST',
+    fetch("/api/auth/signin", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json;charset=utf-8',
+        "Content-Type": "application/json;charset=utf-8",
       },
       body: JSON.stringify(data),
-    })
-      .then((response) => {
-        if (response.ok) {
-          window.location = '/themes';
-        }
-      });
+    }).then((response) => {
+      if (response.ok) {
+        window.location = "/themes";
+      }
+    });
   };
 
   return (
@@ -37,7 +36,9 @@ export default function SignInForm() {
                 placeholder="Enter email..."
               />
             </label>
-            <div id="emailHelp" className="form-text">Well never share your email with anyone else.</div>
+            <div id="emailHelp" className="form-text">
+              Well never share your email with anyone else.
+            </div>
           </div>
           <div className="mb-3">
             <label htmlFor="exampleInputPassword1" className="form-label">
@@ -51,10 +52,19 @@ export default function SignInForm() {
               />
             </label>
           </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
+          <button type="submit" className="btn btn-primary">
+            Войти{" "}
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              window.location = "/auth/signup";
+            }}
+            className="btn btn-primary"
+          >
+            Регистрация
+          </button>
         </form>
-        <div><button type="button" onClick={() => { window.location = '/auth/signup'; }} className="btn btn-primary">Регистрация</button></div>
-
       </div>
     </div>
   );
