92d991020b3f56cb8d718ec7ddfa096fbcf9d373
Geo <geo@Geos-MacBook-Pro.local>
2023-03-10 16:03:42 +0300

    css progressbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,40 @@
-import React, { useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import SignInForm from './Auth/SignInForm';
-import SignUpForm from './Auth/SignUpForm';
-import Header from './Header';
-import MainPage from './MainPage';
-import Themes from './Themes';
-import Profile from './Profile';
-import Words from './Words';
-import Add from './Add';
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import SignInForm from "./Auth/SignInForm";
+import SignUpForm from "./Auth/SignUpForm";
+import Header from "./Header";
+import MainPage from "./MainPage";
+import Themes from "./Themes";
+import Profile from "./Profile";
+import Words from "./Words";
+import Add from "./Add";
 
-export default function App({
-  user, themes, words, learned, allWords,
-}) {
+export default function App({ user, themes, words, learned, allWords }) {
   const [currentUser, setCurrentUser] = useState(user || null);
   return (
     <div>
-      {currentUser && <Header currentUser={currentUser} setCurrentUser={setCurrentUser} />}
+      {currentUser && (
+        <Header currentUser={currentUser} setCurrentUser={setCurrentUser} />
+      )}
       <Routes>
         {/* <Route path="/" element={<SignInForm />} /> */}
         <Route path="/auth/signup" element={<SignUpForm />} />
         <Route path="/auth/signin" element={<SignInForm />} />
-        <Route path="/profile" element={<Profile learned={learned} allWords={allWords} />} />
+        <Route
+          path="/profile"
+          element={
+            <Profile learned={learned} allWords={allWords} themes={themes} />
+          }
+        />
         <Route path="/new" element={<Add themes={themes} />} />
-        <Route path="themes" element={<Themes themes={themes} currentUser={currentUser} />} />
-        <Route path="/:theme" element={<Words words={words} currentUser={currentUser} />} />
-
+        <Route
+          path="themes"
+          element={<Themes themes={themes} currentUser={currentUser} />}
+        />
+        <Route
+          path="/:theme"
+          element={<Words words={words} currentUser={currentUser} />}
+        />
       </Routes>
     </div>
   );
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React from "react";
+import OneThemePr from "./OneThemePr";
 
-export default function Profile({ learned, allWords }) {
+export default function Profile({ learned, allWords, themes, currentUser }) {
   const food = learned.filter((el) => el.Word.theme_id === 1);
   const transport = learned.filter((el) => el.Word.theme_id === 2);
   const meeting = learned.filter((el) => el.Word.theme_id === 3); // Theme
@@ -11,37 +12,49 @@ export default function Profile({ learned, allWords }) {
 
   const allResult = Math.round((learned.length / allWords.length) * 100);
   const resultFood = Math.round((food.length / countFood.length) * 100);
-  const resultTransport = Math.round((transport.length / countTransport.length) * 100);
-  const resultMeeting = Math.round((meeting.length / countMeeting.length) * 100);
+  const resultTransport = Math.round(
+    (transport.length / countTransport.length) * 100
+  );
+  const resultMeeting = Math.round(
+    (meeting.length / countMeeting.length) * 100
+  );
 
-  // console.log(food.length, transport.length, meeting.length, allWords.length);
+  console.log(allResult);
 
-  // console.log(food);
+  // console.log(themes);
   return (
-    <div>
-      <div className="progress" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-        <div className="progress-bar" style={{ width: `${allResult}%` }}>
-          {allResult}
+    <>
+      <div className="row">
+        <h1>Ваш общий прогресс</h1>
+        <div
+          className="progress"
+          role="progressbar"
+          aria-label="Example with label"
+          aria-valuenow="25"
+          aria-valuemin="0"
+          aria-valuemax="100"
+        >
+          <div className="progress-bar" style={{ width: `${allResult}%` }}>
+            {learned.length}/{allWords.length}
+          </div>
         </div>
       </div>
-      <div>Общий прогрес</div>
-      <div className="progress" role="progressbar" aria-label="Success example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-        <div className="progress-bar bg-success" style={{ width: `${resultFood}%` }} />
-        {resultFood}
+      `
+      <div className="row">
+        <OneThemePr theme={themes[0]} result={food.length} count={resultFood} allThe={countFood} />
+        <OneThemePr
+          theme={themes[1]}
+          result={transport.length}
+          count={resultTransport}
+          allThe={countTransport}
+        />
+        <OneThemePr
+          theme={themes[2]}
+          result={meeting.length}
+          count={resultMeeting}
+          allThe={countMeeting}
+        />
       </div>
-
-      <div>Еда</div>
-      <div className="progress" role="progressbar" aria-label="Info example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
-        <div className="progress-bar bg-info" style={{ width: `${resultTransport}%` }} />
-        {resultTransport}
-      </div>
-      <div>Транспорт</div>
-      <div className="progress" role="progressbar" aria-label="Warning example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-        <div className="progress-bar bg-warning" style={{ width: `${resultMeeting}%` }} />
-        {resultMeeting}
-      </div>
-      <div>Встречи</div>
-
-    </div>
+    </>
   );
 }
